a747f6b1d4e58f4313feddf74f4934a6ba19f9e6
etimoshenko <timoshenko.evgeny@gmail.com>
2018-03-06 20:55:39 +0100

    using sets intead of array

--- src/HUD.js
+++ src/HUD.js
@@ -2,11 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 window.addEventListener("load", event => {
-  console.log(document.readyState);
-  let elements = []; // TODO(ET): should be a set
+  let elements = new Set();
   let canvas = document.createElement("canvas");
   let ctx = canvas.getContext("2d");
-  canvas.id = "__HUD";
+  canvas.id = "hud";
   canvas.style.position = "fixed";
   canvas.style.left = 0;
   canvas.style.top = 0;
@@ -20,40 +19,50 @@ window.addEventListener("load", event => {
     paint(ctx, elements);
   });
 
-  observe(el => elements.push(el), () => paint(ctx, elements));
-  document.querySelectorAll(".__X").forEach(el => elements.push(el));
+  observe((added, removed) => {
+    removed.forEach(el => elements.delete(el));
+    added.forEach(el => elements.add(el));
+    paint(ctx, elements);
+  });
+  document.querySelectorAll(".__X").forEach(el => elements.add(el));
   paint(ctx, elements);
 });
 
 function paint(ctx, elements) {
   ctx.clearRect(0, 0, 100000, 100000);
-  for (let i = 0; i < elements.length; ++i) {
-    let box = elements[i].getBoundingClientRect();
+
+  elements.forEach(el => {
+    let box = el.getBoundingClientRect();
     let { left, top, width, height } = box;
     ctx.strokeStyle = "yellow";
     ctx.lineWidth = 2;
     ctx.strokeRect(left, top, width, height);
-  }
+  });
 }
 
-function observe(cb, afterAll) {
-  let elements = [];
+function observe(cb) {
+  let added = new Set();
+  let removed = new Set();
   let mo = new MutationObserver((records, self) => {
     console.log("MutationObserver", records);
     for (let i = 0; i < records.length; ++i) {
       let an = records[i].addedNodes;
-      if (an.length) {
-        for (let j = 0; j < an.length; ++j) {
-          if (an[j].nodeType === Node.ELEMENT_NODE) {
-            let all = an[j].querySelectorAll(".__X");
-            all.forEach(cb);
-          }
+      for (let j = 0; j < an.length; ++j) {
+        if (an[j].nodeType === Node.ELEMENT_NODE) {
+          let all = an[j].querySelectorAll(".__X");
+          all.forEach(el => added.add(el));
+        }
+      }
+      let rn = records[i].removedNodes;
+      for (let j = 0; j < rn.length; ++j) {
+        if (rn[j].nodeType === Node.ELEMENT_NODE) {
+          let all = rn[j].querySelectorAll(".__X");
+          all.forEach(el => removed.add(el));
         }
       }
     }
 
-    afterAll();
+    cb(added, removed);
   });
   mo.observe(document.body, { subtree: true, childList: true });
 }
-
